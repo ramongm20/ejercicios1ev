b105c673c1d25edeb80200a76a5a0704c5a43d09
Ramón García <ramongm20@educastur.es>
2020-10-26 13:49:42 +0100

    Añadiendo entrada del sistema

--- src/unidad2/Conversor.java
+++ src/unidad2/Conversor.java
@@ -1,17 +1,30 @@
 package unidad2;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Conversor {
 
 	public static void main(String[] args) {
 		float euros;
 		float dolares;
 		float precio;
-		
-		euros = 1;
-		precio = 1.18f;
-		dolares = euros * precio;
-		
-		System.out.printf("%.2f", dolares);
+
+		System.out.println("Introduzca cantidad(€):");
+		InputStreamReader isr = new InputStreamReader(System.in);
+		BufferedReader br = new BufferedReader(isr);
+		try {
+			euros = Float.parseFloat(br.readLine());
+			
+			precio = 1.18f;
+			dolares = euros * precio;
+			
+			System.out.printf("%.2f", dolares);
+
+		} catch(IOException ex) {
+			System.out.println("La entrada no es válida.");
+		} // catch
 	} // main
 
-} // Conversor
+}
