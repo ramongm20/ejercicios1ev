cd655fa3cf109280e7364ac69306b9b7337b6b64
Ramón García <ramongm20@educastur.es>
2020-10-31 19:49:37 +0100

    Ejercicio 3 de la unidad 3 resuelto

---
+++ src/unidad3/Bisiesto.java
@@ -0,0 +1,21 @@
+package unidad3;
+
+import java.util.Scanner;
+
+public class Bisiesto {
+
+	public static void main(String[] args) {
+		int anno;
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("Introduce un año:");
+		anno = in.nextInt();
+		
+		if (anno% 4 == 0 && anno % 400 == 0 || anno % 100 != 0) {
+			System.out.println(anno + " es bisiesto.");
+		} else System.out.println(anno + " no es bisiesto.");
+		
+		
+	} // main
+
+}
